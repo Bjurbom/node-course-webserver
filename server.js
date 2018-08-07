8d2e2278a8f015871ae2c6dcec41b62b1759b4d2
tor bjurbom <bjurbom1@gmail.com>
2018-08-07 19:58:52 +0200

    add maintenec for reacher purposes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
 
 var app =  express();
 
@@ -23,11 +24,13 @@ app.use((req,res,next) =>{
     });
     next();
 });
-/*
+
 app.use((req,res,next) =>{
-    res.render('maintenec.hbs');
+    res.render('maintenec.hbs', {
+        message: 'The website is down because Im working on it will be coming up sometime in the near future, stay tuned'
+    });
 });
-*/
+
 app.use(express.static(__dirname + '/publc'));
 
 hbs.registerHelper('getCurrentYear', () =>{
